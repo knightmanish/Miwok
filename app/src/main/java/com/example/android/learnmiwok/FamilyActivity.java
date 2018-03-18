584be05e1f76b02cc5279c89fe6a3138fb6504f5
Manish Arya <knightmanish@gmail.com>
2018-03-18 13:47:16 +0100

    1) new icon showed 2) round icon deleted 3) activites linked with intent 4) activity title set in manifest

--- app/src/main/java/com/example/android/learnmiwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/learnmiwok/FamilyActivity.java
@@ -9,5 +9,6 @@ public class FamilyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_family);
+//        setTitle("Family");
     }
 }
--- app/src/main/java/com/example/android/learnmiwok/MainActivity.java
+++ app/src/main/java/com/example/android/learnmiwok/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.android.learnmiwok;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbers(View view) {
+//        Log.i("MainActiviy","in openNumbers");
+        Intent intent = new Intent(this, NumbersActivity.class);
+        if (intent != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void openColors(View view) {
+        Intent intent = new Intent(this, ColorsActivity.class);
+        if (intent != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void openFamily(View view) {
+        Intent intent = new Intent(this, FamilyActivity.class);
+        if (intent != null) {
+            startActivity(intent);
+        }
+    }
+
+    public void openPhrases(View view) {
+        Intent intent = new Intent(this, PhrasesActivity.class);
+        if (intent != null) {
+            startActivity(intent);
+        }
     }
 }

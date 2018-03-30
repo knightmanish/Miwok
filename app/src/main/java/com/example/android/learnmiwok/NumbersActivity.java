83063f8be36f3a8eff58eed7358f82a057ddb47b
Manish Arya <knightmanish@gmail.com>
2018-03-30 22:56:49 +0100

    shows grid layout

--- app/src/main/java/com/example/android/learnmiwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/learnmiwok/NumbersActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.GridView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -30,7 +31,7 @@ public class NumbersActivity extends AppCompatActivity {
         ArrayAdapter<String> itemsAdapter =
                 new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, numbersList);
 
-        ListView listView = (ListView) findViewById(R.id.list);
-        listView.setAdapter(itemsAdapter);
+        GridView gridView = (GridView) findViewById(R.id.grid);
+        gridView.setAdapter(itemsAdapter);
     }
 }
